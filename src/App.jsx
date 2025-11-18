a16d4fb785e43076f5cd9c7f514e6bf4a2fa23fb
Lohran Mendes <lohranfm@gmail.com>
2025-11-17 21:39:52 -0300

    feat: update title and add app heading for book management system

--- src/App.jsx
+++ src/App.jsx
@@ -42,29 +42,9 @@ function App() {
   }
 
   return (
-    // <div className="AppComponent">
-    // <h1>Ao rodar o projeto é possível verificar as questões adicionando os caminhos na url de cada rota que é mencionada no arquivo main.jsx, ou clicando nos links abaixo:</h1>
-    // <ul>
-    //   <li><a href="/question_01">Questão 01</a></li>
-    //   <li><a href="/question_02">Questão 02</a></li>
-    //   <li><a href="/question_03">Questão 03</a></li>
-    //   <li><a href="/question_04">Questão 04</a></li>
-    //   <li><a href="/question_05">Questão 05</a></li>
-    //   <li><a href="/question_06">Questão 06</a></li>
-    //   <li><a href="/question_07">Questão 07</a></li>
-    //   <li><a href="/question_08">Questão 08</a></li>
-    //   <li><a href="/question_09">Questão 09</a></li>
-    //   <li><a href="/question_10">Questão 10</a></li>
-    //   <li><a href="/question_11">Questão 11</a></li>
-    //   <li><a href="/question_12">Questão 12</a></li>
-    //   <li><a href="/question_13">Questão 13</a></li>
-    //   <li><a href="/question_14">Questão 14</a></li>
-    //   <li><a href="/question_15">Questão 15</a></li>
-    //   <li><a href="/question_16">Questão 16</a></li>
-    // </ul>
-    // </div>
     <>
       <Header />
+      <h1 className="app-heading">Sistema de Cadastro de Livros</h1>
       <BookList
         livros={livros}
         aoRemoverLivro={manipulaRemocaoDeLivro}
